8c54781395a1176f07c6f6db096c41191efe1b8c
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-06-16 23:09:41 +0200

    Attach command to geonature command

--- api2gn/blueprint.py
+++ api2gn/blueprint.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 
+from api2gn.commands import cmd_list_parsers, run
 
-blueprint = Blueprint("api2gn", __name__)
+blueprint = Blueprint("parser", __name__)
+
+blueprint.cli.add_command(cmd_list_parsers)
+blueprint.cli.add_command(run)
 
 from api2gn.admin import *
---
+++ api2gn/commands.py
@@ -0,0 +1,20 @@
+import sys
+
+import click
+
+
+from api2gn.utils import list_parsers, get_parser
+
+
+@click.command(name="list")
+def cmd_list_parsers():
+    parsers = list_parsers()
+    for p in parsers:
+        click.secho(f"🌵 {p}", fg="green")
+
+
+@click.command()
+@click.argument("name")
+def run(name):
+    Parser = get_parser(name)
+    Parser().run()
