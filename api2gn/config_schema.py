dfd035efde91429d28438975c6b4ca8591fd08df
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-06-19 13:55:34 +0200

    add parameters for parsers http get

--- api2gn/config_schema.py
+++ api2gn/config_schema.py
@@ -2,4 +2,6 @@ from marshmallow import Schema, fields
 
 
 class Api2GNSchema(Schema):
-    PARSER_NUMBER_OF_TRIES = fields.Integer(missing=5)
+    PARSER_NUMBER_OF_TRIES = fields.Integer(load_default=5)
+    PARSER_RETRY_SLEEP_TIME = fields.Integer(load_default=5)
+    PARSER_RETRY_HTTP_STATUS = fields.List(fields.Integer(), load_default=[503])
